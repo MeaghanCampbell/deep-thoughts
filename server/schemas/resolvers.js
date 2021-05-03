e515b6f0571c0b76b8f6a7c384b973d02b0bc49c
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-05-03 18:13:00 -0500

    queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,36 @@
 // resolvers are functions we connect to each query or mutation typeDef that performs crud actions that the query or mutation can execute
 
+// import Thought & User models
+const { User, Thought } = require('../models')
+
 const resolvers = {
+    // query thoughts and perform a find () on the tought model
     Query: {
-        helloWorld: () => {
-            return 'Hello World!'
+        // get all thoughts or get all thoughts attached to a specific username
+        // pass parent as a placeholder param - need something in the first spot so we can access username in second spot
+        thoughts: async (parent, { username }) => {
+            // ternary operator to see if username exists, if it does set params to an object with username key to that value set, if not return empty object
+            const params = username ? { username } : {};
+            // pass object with or without data into find and if there's data lookup by a username
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+        // get thought by id
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id })
+        },
+        //get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts')
+        },
+        // get user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts')
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,11 +5,42 @@ const { gql } = require('apollo-server-express')
 
 // create our typeDefs
 // tagged template funciton
-// query data type - query name is helloWorld and data type is a string
+// query data type - instruct query to return array of Thought data
+// (username:string) - update query that could receive a parameter if we wanted
+// ! after string notitates that data must exist for query to be carried out
 const typeDefs = gql`
-type Query {
-    helloWorld: String
-}
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type User {
+      _id: ID
+      username: String
+      email: String
+      friendCount: Int
+      thoughts: [Thought]
+      friends: [User]
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought 
+  }
 `;
 
 // export typeDefs

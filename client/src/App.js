633a8e00a59d75bc06519be708ba89054896ddae
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-05-05 10:59:21 -0500

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,14 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
+// import browser router and route components from the React Router library 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // establish new connection to the graphQL server using Apollo
 // React runs on 3000 and GraphQL runs on 3001
@@ -20,13 +28,24 @@ function App() {
   return (
     // pass client variable in for the value of the client prop in the provider
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            {/* wrapped all route components in switch to render no match if the route isn't any of the proceeding paths */}
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+// link component for nav so you can change the URL while staying on the same page
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1>Deep Thoughts</h1>
+        <Link to="/">
+          <h1>Deep Thoughts</h1>
+        </Link>
+
+        <nav className="text-center">
+          {/* uses to instead of href */}
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </nav>
       </div>
     </header>
   );
